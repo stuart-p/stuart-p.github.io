ac64d8ff1fb940d5625d8ce0f3a5dc5fbf1a9c16
stuart-p <stuart_palmer85@hotmail.com>
2020-04-28 17:20:44 +0100

    initial commit

---
+++ src/components/Hero/HeroBG.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HeroBG = () => {
+  return (
+    <div id="heroBG">
+      <div className="bgTriangleTop"></div>
+      <div className="bgTriangleMid"></div>
+      <div className="bgTriangleBot"></div>
+    </div>
+  );
+};
+
+export default HeroBG;
--- src/mock/data.js
+++ src/mock/data.js
@@ -9,18 +9,21 @@ export const headData = {
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: "Hi, I'm ",
+  name: 'Stuart',
+  subtitle: "I'm a fullstack developer",
+  cta: 'Contact Me',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
+  img: 'profilePic.jpg',
+  paragraphOne:
+    'I started my career as a mechanically-trained systems engineer; my role being to develop deep domain understanding, and use this to guide effective product development. This naturally led to me increasingly focussing on leading complex engineering projects spanning multiple technical disciplines.',
+  paragraphTwo:
+    "I've led engineering teams in designing complex robots, software interfaces and military equipment. I have also been responsible for wider technical leadership within businesses, such as resource planning and management, product development roadmaps, and business strategy.",
+  paragraphThree:
+    ' I am now focussed more on the software side of engineering, including  web- and app- development. Interested in both front-line engineering roles and leadership positions.',
   resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
 };
 
@@ -28,8 +31,8 @@ export const aboutData = {
 export const projectsData = [
   {
     id: uuidv1(),
-    img: 'project.jpg',
-    title: '',
+    img: 'heroesOfRealm.png',
+    title: 'Heroes of the Realm',
     info: '',
     info2: '',
     url: '',
@@ -37,8 +40,8 @@ export const projectsData = [
   },
   {
     id: uuidv1(),
-    img: 'project.jpg',
-    title: '',
+    img: 'qwizardsPreviewImg.png',
+    title: 'Qwizards: Arena',
     info: '',
     info2: '',
     url: '',
@@ -46,8 +49,17 @@ export const projectsData = [
   },
   {
     id: uuidv1(),
-    img: 'project.jpg',
-    title: '',
+    img: 'screenshot.png',
+    title: 'Saw It!',
+    info: '',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  },
+  {
+    id: uuidv1(),
+    img: 'mazeOfMagicBlock.png',
+    title: 'Maze Of Magic',
     info: '',
     info2: '',
     url: '',
